5eb61a262b230733bcc6a227a49af57c9fae1cbe
Dexter Lohnes <dexterlohnes@gmail.com>
2021-02-23 10:45:04 -0800

    Implement iOS!

--- iosApp/iosApp/Views/RocketLaunchRow.swift
+++ iosApp/iosApp/Views/RocketLaunchRow.swift
@@ -7,15 +7,43 @@
 //
 
 import SwiftUI
+import shared
 
 struct RocketLaunchRow: View {
+    var rocketLaunch: RocketLaunch
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack() {
+            VStack(alignment: .leading, spacing: 10.0) {
+                Text("Launch name: \(rocketLaunch.missionName)")
+                Text(launchText).foregroundColor(launchColor)
+                Text("Launch year: \(rocketLaunch.launchYear)")
+                Text("Launch details: \(rocketLaunch.details ?? "None")")
+                
+            }
+        }
+    }
+}
+
+extension RocketLaunchRow {
+    private var launchText: String {
+        if let isSuccess = rocketLaunch.launchSuccess {
+            return isSuccess.boolValue ? "Successful" : "Unsuccessful"
+        } else {
+            return "No data"
+        }
+    }
+    
+    private var launchColor: Color {
+        if let isSuccess = rocketLaunch.launchSuccess {
+            return isSuccess.boolValue ? Color.green : Color.red
+        } else {
+            return Color.gray
+        }
     }
 }
 
 struct RocketLaunchRow_Previews: PreviewProvider {
     static var previews: some View {
-        RocketLaunchRow()
+        RocketLaunchRow(rocketLaunch: RocketLaunch(flightNumber: 001, missionName: "Test mission", launchYear: 2001, launchDateUTC: "Date str", rocket: Rocket(id: "1", name: "Test rocket", type: "Falcon Test"), details: nil, launchSuccess: true, links: Links.init(missionPatchUrl: nil, articleUrl: nil)))
     }
 }
