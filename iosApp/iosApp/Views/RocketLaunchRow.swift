26c52d7f019dc8941c2569d8b9ae82c58f105c87
Dexter Lohnes <dexterlohnes@gmail.com>
2021-02-23 10:44:25 -0800

    Stop tracking stuff

---
+++ iosApp/iosApp/Views/RocketLaunchRow.swift
@@ -0,0 +1,21 @@
+//
+//  RocketLaunchRow.swift
+//  iosApp
+//
+//  Created by Dexter Lohnes on 2/23/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct RocketLaunchRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RocketLaunchRow_Previews: PreviewProvider {
+    static var previews: some View {
+        RocketLaunchRow()
+    }
+}
